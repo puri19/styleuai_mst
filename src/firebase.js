088649180611923c3196183ab2cc492d6e1f0bae
puri19 <77183948+puri19@users.noreply.github.com>
2025-03-06 17:37:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyATGGcQ6phQi3FT66XwS_ADGcgmUh8cbgA",
-    authDomain: "userauth-47b3b.firebaseapp.com",
-    projectId: "userauth-47b3b",
-    storageBucket: "userauth-47b3b.firebasestorage.app",
-    messagingSenderId: "1066279463530",
-    appId: "1:1066279463530:web:520a02a9fa2e4448c8b813",
-    measurementId: "G-5085F2CBVR"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
